6ba8f360583a86cb9f0617b5575d69d634901585
roybarak80 <barakroy@gmail.com>
2019-09-21 17:17:19 +0300

    make it better

--- client/src/actions/sitesActions.js
+++ client/src/actions/sitesActions.js
@@ -1,189 +1,171 @@
 
-// import { toast } from "react-toastify";
-// import helpers from '../helpers/Helpers';
+import { toast } from "react-toastify";
+import helpers from '../helpers/Helpers';
 
-// export const checkForCityCode = (cityCodeObj) => {
+export const checkForCityCode = (cityCodeObj) => {
 
-//     return async dispatch => {
+    return async dispatch => {
 
-//         fetch(`/getCityKeyByGeoPosition/${cityCodeObj.latitude}/${cityCodeObj.longitude}`)
-//             .then(function (response) {
+        fetch(`/getCityKeyByGeoPosition/${cityCodeObj.latitude}/${cityCodeObj.longitude}`)
+            .then(function (response) {
 
-//                 return response.json();
-//             })
-//             .then(function (data) {
-//                 return dispatch({
-//                     type: "CHECK_FOR_CITYCODE",
-//                     payload: data.Key
-//                 });
-//             });
-//     };
+                return response.json();
+            })
+            .then(function (data) {
+                return dispatch({
+                    type: "CHECK_FOR_CITYCODE",
+                    payload: data.Key
+                });
+            });
+    };
 
 
-// }
+}
 
-// /**
-//  * Checks if selected city is favorite
-//  *
-//  * @param {number} cityCode
-//  */
-// export const checkForFavorite = (cityCode) => {
+/**
+ * Checks if selected city is favorite
+ *
+ * @param {number} cityCode
+ */
+export const checkForFavorite = (cityCode) => {
 
-//     return {
-//         type: "CHECK_FOR_FAVORITE",
-//         payload: cityCode
-//     };
-// }
+    return {
+        type: "CHECK_FOR_FAVORITE",
+        payload: cityCode
+    };
+}
 
-// export const getCurrentWeather = (prmCityCode) => {
+export const getCurrentWeather = (prmCityCode) => {
 
-//     return async dispatch => {
+    return async dispatch => {
 
-//         fetch(`/getCurrentWeather/${prmCityCode}`)
-//             .then(function (response) {
-//                 return response.json();
-//             })
-//             .then(function (data) {
-//                 return dispatch({
-//                     type: "GET_CURRENT_WEATHER",
-//                     payload: data
-//                 });
-//             });
-//     };
-// };
+        fetch(`/getCurrentWeather/${prmCityCode}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                return dispatch({
+                    type: "GET_CURRENT_WEATHER",
+                    payload: data
+                });
+            });
+    };
+};
 
-// export const getCurrentForecast = (prmCityCode, prmIsMetricUnits) => {
+export const getCurrentForecast = (prmCityCode, prmIsMetricUnits) => {
 
-//     return async dispatch => {
+    return async dispatch => {
 
-//         fetch(`/getCurrentForecast/${prmCityCode}/${prmIsMetricUnits}`)
-//             .then(function (response) {
+        fetch(`/getCurrentForecast/${prmCityCode}/${prmIsMetricUnits}`)
+            .then(function (response) {
 
-//                 return response.json();
-//             })
-//             .then(function (data) {
-//                 return dispatch({
-//                     type: "GET_CURRENT_FORECAST",
-//                     payload: data
-//                 });
-//             });
-//     };
-// };
+                return response.json();
+            })
+            .then(function (data) {
+                return dispatch({
+                    type: "GET_CURRENT_FORECAST",
+                    payload: data
+                });
+            });
+    };
+};
 
-// export const toggleWeatherUnits = (isMetricUnits) => {
+export const toggleWeatherUnits = (isMetricUnits) => {
 
-//     isMetricUnits = !isMetricUnits
-//     return {
-//         type: "TOGGLE_WEATHER_UNITS",
-//         payload: isMetricUnits
-//     };
-// }
+    isMetricUnits = !isMetricUnits
+    return {
+        type: "TOGGLE_WEATHER_UNITS",
+        payload: isMetricUnits
+    };
+}
 
-// export const onSelectCity = (textFeldValue) => {
+export const onSelectCity = (textFeldValue) => {
 
-//     return {
-//         type: "SELECT_CITY_FORECAST",
-//         payload: textFeldValue
-//     };
-// }
+    return {
+        type: "SELECT_CITY_FORECAST",
+        payload: textFeldValue
+    };
+}
 
-// export const searchCityAutoComplete = searchText => {
+export const searchCityAutoComplete = searchText => {
 
-//     var validationReg = /^[A-Za-z\s]*$/;
+    var validationReg = /^[A-Za-z\s]*$/;
 
-//     if (!validationReg.test(searchText)) {
-//         toast.warn("Only English Letters Are Allowed");
-//         return {
-//             type: "FETCH_DATA_ERR"
-//         };
+    if (!validationReg.test(searchText)) {
+        toast.warn("Only English Letters Are Allowed");
+        return {
+            type: "FETCH_DATA_ERR"
+        };
 
-//     } else {
-//         return async dispatch => {
+    } else {
+        return async dispatch => {
 
-//             fetch(`/searchCityAutoComplete/${searchText}`)
-//                 .then(function (response) {
+            fetch(`/searchCityAutoComplete/${searchText}`)
+                .then(function (response) {
 
-//                     return response.json();
-//                 })
-//                 .then(function (data) {
-//                     return dispatch({
-//                         type: "AUTOCOMPLETE_SEARCH",
-//                         payload: data
-//                     });
-//                 });
-//         };
-//     }
+                    return response.json();
+                })
+                .then(function (data) {
+                    return dispatch({
+                        type: "AUTOCOMPLETE_SEARCH",
+                        payload: data
+                    });
+                });
+        };
+    }
 
-// }
-// export const addForecastToFavorites = (currCityCode, currWeather, currTempUnits) => {
+}
+export const addForecastToFavorites = (currCityCode, currWeather, currTempUnits) => {
 
-//     const currWeatherObj = helpers.getWeatherFilteredDataObj(currWeather, currTempUnits);
-//     const newFavoriteObj = Object.assign({}, currWeatherObj, {
-//         id: currCityCode,
-//         name: currWeatherObj.WeatherCity
-//     });
+    const currWeatherObj = helpers.getWeatherFilteredDataObj(currWeather, currTempUnits);
+    const newFavoriteObj = Object.assign({}, currWeatherObj, {
+        id: currCityCode,
+        name: currWeatherObj.WeatherCity
+    });
 
-//     delete newFavoriteObj.WeatherCity;
+    delete newFavoriteObj.WeatherCity;
 
-//     return {
-//         type: "ADD_TO_FAVORITES",
-//         payload: newFavoriteObj
-//     };
+    return {
+        type: "ADD_TO_FAVORITES",
+        payload: newFavoriteObj
+    };
 
-// }
+}
 
-// export const deleteForecastFromFavorites = (favoriteID) => {
+export const deleteForecastFromFavorites = (favoriteID) => {
 
-//     return {
-//         type: "DELETE_FROM_FAVORITES",
-//         payload: favoriteID
-//     };
-// }
+    return {
+        type: "DELETE_FROM_FAVORITES",
+        payload: favoriteID
+    };
+}
 
-// export const toggleSideMenu = (currSideMenuState) => {
+export const toggleSideMenu = (currSideMenuState) => {
 
-//     currSideMenuState = !currSideMenuState;
-//     return {
-//         type: "SET_SIDEMENU_STATE",
-//         payload: currSideMenuState
-//     };
-// }
+    currSideMenuState = !currSideMenuState;
+    return {
+        type: "SET_SIDEMENU_STATE",
+        payload: currSideMenuState
+    };
+}
 
-// export const toggleThemeType = (currThemeType) => {
+export const toggleThemeType = (currThemeType) => {
 
-//     currThemeType = !currThemeType;
-//     return {
-//         type: "TOGGLE_THEME_TYPE",
-//         payload: currThemeType
-//     };
-// }
+    currThemeType = !currThemeType;
+    return {
+        type: "TOGGLE_THEME_TYPE",
+        payload: currThemeType
+    };
+}
 
-// export const getForecastByCityCode = (favoriteCitiesNames) => {
+export const getForecastByCityCode = (favoriteCitiesNames) => {
 
-//     return {
-//         type: "FETCHING_DATA",
-//         payload: favoriteCitiesNames
-//     };
+    return {
+        type: "FETCHING_DATA",
+        payload: favoriteCitiesNames
+    };
 
-// }
+}
 
-// // export const updateSearchField = (cityName) => {
-// //     // Check if input is OK
-// //     var validationReg = /^[A-Za-z\s]*$/;
-// //     let isValid = true;
-// //     // if it's not OK, update the isNameValid state
-// //     if (!validationReg.test(cityName)) {
-// //         isValid = false;
-
-// //     }
-// //     const data = {
-// //         cityName: cityName,
-// //         isSearchFieldValid: isValid
-// //     };
-// //     return {
-// //         type: "UPDATE_SEARCH_FIELD",
-// //         payload: data
-// //     };
-// // }
 
 
